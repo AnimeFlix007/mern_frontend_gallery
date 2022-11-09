0fbfca4ab8a0e2545ebc555c4c92b4451080710f
devjitbose111 <devjitbose111@gmail.com>
2022-11-09 23:51:41 +0530

    upload pics

--- src/context/store/store.js
+++ src/context/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userSlice from "../slices/user/userSlice";
+import gallerySlice from "../slices/gallery/gallerySlice";
 
 const reducers = combineReducers({
   users: userSlice,
+  gallery: gallerySlice
 });
 
 const store = configureStore({
