e10c12a6ea20c7ec669d046e9ce49971d1bc1fc0
devjitbose111 <devjitbose111@gmail.com>
2022-11-10 15:38:46 +0530

    disableReactDevTools

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./context/store/store";
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
